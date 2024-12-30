912813441eab99ca3363406c857c1b9bae7fef70
tholho <tholhodev@gmail.com>
2024-12-30 16:24:13 +0100

    groups logic for game loop

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for entity in updatable:
+            entity.update(dt)
         screen.fill("#000000")
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting asteroids!")
