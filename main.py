f3fb0cf1cf07d6251680c33e90c0d231ece43ddf
tholho <tholhodev@gmail.com>
2024-12-30 15:42:16 +0100

    init screen + exit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")
+        pygame.display.flip()
+    print("Starting asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+
+if __name__ == "__main__":
+    main()
